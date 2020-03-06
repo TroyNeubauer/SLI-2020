ed3741d640bd6290d746bfd4be3331341d71bc35
Troy Neubauer <troyneubauer@gmail.com>
2020-03-05 21:56:55 -0500

    hardcoded offsetof() for comparability

--- Core/include/Packet.h
+++ Core/include/Packet.h
@@ -67,7 +67,7 @@ struct PacketHeader
 
 };
 
-static_assert(offsetof(PacketHeader, CRC32) == 0, "CRC must start at offset 0");
+static_assert( &(((PacketHeader*) nullptr)->CRC32) == 0, "CRC must start at offset 0");
 
 
 const int MAX_PACKET_DATA_SIZE = 256;
